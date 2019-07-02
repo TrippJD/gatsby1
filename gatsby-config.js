92ddd79231613f449086c8f49213497b99755f07
Tripp <tripp4414@yahoo.com>
2019-07-01 22:16:33 -0600

    netlify plugin

--- gatsby-config.js
+++ gatsby-config.js
@@ -41,10 +41,10 @@ module.exports = {
       options: {
         // Add any options here
       },
-      plugins: [`gatsby-plugin-netlify`],
     },
     // this (optional) plugin enables Progressive Web App + Offline functionality
     // To learn more, visit: https://gatsby.dev/offline
     // `gatsby-plugin-offline`,
+    `gatsby-plugin-netlify`,
   ],
 }
